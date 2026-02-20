f35d9ae186e5791c6de792a274e689c5054328ca
ResarfGaming <contactresarfgaming@gmail.com>
2026-02-20 18:56:41 +0000

    Update test.js

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).send('hello world');
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
